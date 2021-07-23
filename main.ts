f6d6a84b90560f2020b87ed8e56b93f9c9ca04fa
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 15:50:18 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Apa")
+    basic.showNumber(Apa)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Anya")
+    basic.showNumber(Anya)
+})
+let Anya = 0
+let Apa = 0
+Apa = 61556754
+Anya = 28554325
